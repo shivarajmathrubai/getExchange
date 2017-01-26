0073d4457e55b58f0f9e23f3e42ebb74e90da33c
shivarajem <me.shivaraj@gmail.com>
2017-01-26 17:33:46 +0000

    Added Multi currency support Added Amout support Added counter and request logging in Redis

--- exchange.py
+++ exchange.py
@@ -3,8 +3,9 @@ from flask import request
 from flask import json
 import requests
 import logging
+import redis
 
-import re
+import random
 
 logging.basicConfig(filename='exchange-webhook.log',level=logging.DEBUG, format='%(asctime)s %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -15,28 +16,76 @@ app = Flask(__name__)
 def webhook():
     logger.info("### Webhook called ###")
     req = request.get_json(silent=True, force=True)
-    cur = req['result']['parameters']
-    k = getINR(cur['currency-name'])
+    logreq(req)
+    intent = getIntent(req)
+    if intent == "getExhangeRate":
+        cur =(req['result']['parameters']['currency-name'], 'INR', '1')
+        k = (getMultiCur(cur),'INR')
+    elif intent == "multiCurrency":
+        cur = (req['result']['parameters']['input-currency'], req['result']['parameters']['output-currency'], '1')
+        k = (getMultiCur(cur),req['result']['parameters']['output-currency'])
+    elif intent == "withamount":
+        cur = (req['result']['parameters']['unit-currency']['currency'], req['result']['parameters']['currency-name'], req['result']['parameters']['unit-currency']['amount'])
+        k = (getMultiCur(cur),req['result']['parameters']['currency-name'])
+    else:
+        k = ("Oppppsss.... ","Something really Bad happened.. Try that agian ?")
     return formresponse(k)
 
+def logreq(req):
+    try:
+        logger.info("### Initiating REDIS Connection ####")
+        r = redis.StrictRedis(host='localhost', port=6379, db=1)
+        r.ping()
+    except Exception:
+        logging.exception("### Could not connect to REDIS ###")
+        pass
 
-def getINR(cur):
-    payload = {'a':'1', 'from':cur, 'to':'INR'}
+    if r.exists("COUNTER"):
+        pass
+    else:
+        r.set("COUNTER",0)
+    unq = r.incr("COUNTER",1)
+    r.hset("REQUEST",unq,req)
+    logger.debug("### Counter from redis is {} ####".format(r.get("COUNTER")))
+    logger.info("### Request registered in REDIS###")
+
+def getIntent(req):
+    logger.info("### Checking Intent Name ###")
+    try:
+        intent = req['result']['metadata']['intentName']
+        if intent == "getExhangeRate":
+            return intent
+        elif intent == "multiCurrency":
+            return intent
+        elif intent == "withamount":
+            return intent
+    except(TypeError,ValueError,Exception):
+        logger.error("### Input was not in the right format ###")
+
+def getMultiCur(intup):
+    #print(intup)
+    incur,outcur,amt = intup #tuple
+    logger.info("### Multi-Currency Flow triggered ###")
+    payload = {'a':amt, 'from':incur, 'to':outcur}
     fullhtml = requests.get(url, params=payload)
-    #print(fullhtml.url)
     a = fullhtml.text
     b = a.split()[-14]
-    #print(b)
     v = b.split('>')[1]
     return v
 
 def formresponse(k):
+    val,curr = k
+    responses = ["Okay, it's {} {}".format(val,curr),
+                 "The current rate is {} {}".format(val,curr),
+                 "The exchange rate is {} {}".format(val,curr),
+                 "it's {} {}".format(val,curr)]
+    d = random.choice(responses)
     data = {}
-    data['speech'] = k
-    data['displayText'] = k
+    data['speech'] = d
+    data['displayText'] = d
     data['data'] = ''
     data['contextOut'] = ''
-    data['source'] = 'getExchange'
+    data['source'] = 'TPYBOT'
     jsondata = json.dumps(data)
     final = make_response(jsondata)
     final.headers['Content-Type'] = "application/json"
